b0224a3b4376d63fd275df01cf94af6a19a4a726
yusuke39 <hiranoyusuke39@gmail.com>
2020-12-30 18:15:56 +0900

    バリデーション作成＆コンポーネント化

--- front/client/src/components/login/Login.js
+++ front/client/src/components/login/Login.js
@@ -1,7 +1,7 @@
-import React from "react"
-import Header from "../Header"
-import Button from '@material-ui/core/Button';
-import "../css/LoginSub.css"
+import React from "react";
+import Header from "../Header";
+import LoginValid from "./LoginVarid"
+import "../css/LoginSub.css";
 
 
 class Login extends React.Component {
@@ -13,20 +13,7 @@ class Login extends React.Component {
         <div className="login-box">
           <p className="login-box-title">Share Tech</p>
           <p className="login-box-word">ログイン</p>
-          <form className="login-box-form">
-            <label className="login-box-label">メールアドレス<br/>
-              <input className="login-box-input"></input>
-            </label>
-            <label className="login-box-label">パスワード<br/>
-              <input className="login-box-input"></input>
-            </label>
-            <a href="/aaa" className="forgot-password-link">パスワードを忘れた方はこちら</a>
-            <Button 
-              variant="outlined" 
-              style={{ color: "#e0f2f1", backgroundColor: "black" ,position: "relative", top: "40px", left: "-215px"}}>
-            ログイン
-            </Button>
-          </form>
+          <LoginValid />
           <a href="/aaa" className="create-new-account-link">アカウント作成はこちら</a>
         </div>
       </div>
